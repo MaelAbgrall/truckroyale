ada834135a334d0137f59e9a9d0f437b0e9659ec
Adrien BAUDE <Vendicare@localhost.localdomain>
2017-11-10 15:50:23 +0100

    commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,19 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    this.get('store').push({
-      data: [{
-        id: 1,
-        type: 'truck',
-        attributes: {
-          name: 'truckpatate'
-        },
-        relationships: {}
-      }]
+    let truck = this.get('store').createRecord('truck', {
+      id: 1,
+      name: 'TruckPatate'
     });
+    truck.save();
     return {
       user: {name: 'userName', money: '30'},
-      truckList: [this.get('store').findRecord('truck', 1)],
+      truckList: this.get('store').findAll('truck'),
       typeList: ['type1', 'type2', 'type3'],
       journeyList: [{name: 'journey1'}, {name: 'journey2'}, {name: 'journey3'}]
     };
